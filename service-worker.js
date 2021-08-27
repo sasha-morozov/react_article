472a00beb337c14c397ce5571d6462357363be89
Oleksandr Morozov <oleksandr.morozov@protonmail.com>
2021-08-27 17:14:40 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.96df153bb7a284ee3c767076fdb10c4f.js"
 );
 
 self.addEventListener('message', (event) => {
